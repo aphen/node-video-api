d8ac3a7f27d8b6a4f35e7cd78b4d122c0e23eadf
aphen <aphen@163.com>
2021-04-27 18:38:12 +0800

    video api变更

--- models/videos.js
+++ models/videos.js
@@ -3,6 +3,8 @@ var VideosSchema = new mongoose.Schema({
     name: String,
     genre: String,
     description: String,
+    address: String,
+    age: Number,
     updated_at: {
         type: Date,
         default: Date.now
