13f7d16ad4b893ff79143634d113303b8a2fd218
aphen <aphen@163.com>
2021-05-21 17:15:03 +0800

    登录功能完善

--- utils/checkToken.js
+++ utils/checkToken.js
@@ -8,35 +8,36 @@ let tokenYZ = async function (req, res, next) {
         return
     }
     //app.get('secret')
-    let con = await jwt.verify(token, 'qwert');
+    let con;
     console.log(con);
-    // try {
-    //     return {
-    //         id: con.id,
-    //         token: true
-    //     }
-    // }
-    // catch (err) {
-    //     console.log(err);
-    //     if (err.name == 'TokenExpiredError') {//token过期
-    //         let str = {
-    //             iat: 1,
-    //             exp: 0,
-    //             msg: 'token过期'
-    //         }
-    //         return str;
-    //     } else if (err.name == 'JsonWebTokenError') {//无效的token
-    //         let str = {
-    //             iat: 1,
-    //             exp: 0,
-    //             msg: '无效的token'
-    //         }
-    //         return str;
-    //     }
-    // }
+    try {
+        con = await jwt.verify(token, 'qwert');
+        // return {
+        //     id: con.id,
+        //     token: true
+        // }
+    } catch (err) {
+        console.log(err);
+        if (err.name == 'TokenExpiredError') {//token过期
+            con = {
+                iat: 1,
+                exp: 0,
+                msg: 'token过期'
+            }
+           // return str;
+        } else if (err.name == 'JsonWebTokenError') {//无效的token
+            con = {
+                iat: 1,
+                exp: 0,
+                msg: '无效的token'
+            }
+            //return str;
+        }
+        //return next(err);
+    }
 
 
-    // jwt.verify(token, app.get('secret'), (err, decoded) => {
+    // jwt.verify(token, 'qwert', (err, decoded) => {
     //     if (err) {
     //         console.log(err);
     //         if (err.name == 'TokenExpiredError') {//token过期
@@ -64,7 +65,7 @@ let tokenYZ = async function (req, res, next) {
     if (con.iat < con.exp) {//开始时间小于结束时间，代表token还有效
         const { id } = con    //token解码后是否存在id，此id在生成token时已定义好
         if (!id) {
-            res.status(401).send("token无效")
+            res.status(401).send("token已过期")
             return
         }
 
@@ -79,7 +80,7 @@ let tokenYZ = async function (req, res, next) {
     } else {
         console.log('3333')
         res.status(401).send(con.msg)
-        return next();
+        return;
     }
 }
 
